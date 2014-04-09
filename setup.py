171083d3d6e77304c0ae84dc160640b70fe0bb4d
Lex Toumbourou <lextoumbourou@gmail.com>
2014-04-09 21:46:05 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from pip.req import parse_requirements
+from setuptools import setup
+
+parsed_reqs = parse_requirements('requirements.txt')
+reqs = [str(pr.req) for pr in parsed_reqs]
+
+setup(
+    name='simple-ranker',
+    version='1.0.0',
+    author='Lex Toumbourou',
+    author_email='lextoumbourou@gmail.com',
+    description='An extremely simple, multi-value ranking module for pandas DataFrames',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+    ],
+    packages=['simple_ranker'],
+    package_dir={'simple_ranker': 'simple_ranker'},
+    url='https://github.com/lextoumbourou/simple-ranker',
+    install_requires=reqs
+)
